4a54863532f916c222db9e0ab7e2e4f973cf9e4c
ValenGonzalez03 <111811012+ValenGonzalez03@users.noreply.github.com>
2022-10-24 04:16:57 -0300

    iteradores internos, externos y de rango (con cosas a corregir probablemente)

--- abb.go
+++ abb.go
@@ -1,5 +1,9 @@
 package diccionario
 
+import (
+	TDAPila "diccionario/pila"
+)
+
 const (
 	VALOR_CMP = 0
 )
@@ -22,17 +26,18 @@ type nodoAb[K comparable, V any] struct {
 }
 
 type iteradorDict[K comparable, V any] struct {
-	diccionario
-	actual
-	desde
-	hasta
+	diccionario   *ab[K, V]
+	actual        *nodoAb[K, V]
+	rangoMin      *K
+	rangoMax      *K
+	pilaElementos TDAPila.Pila[*nodoAb[K, V]]
 }
 
 // ##############################################################################################################
 
-func CrearABB[K comparable, V any](funcion_cmp func(K, K) int) DiccionarioOrdenado[K, V] {
+func CrearABB[K comparable, V any](funcion_cmp funcCmp[K]) DiccionarioOrdenado[K, V] {
 	dict := new(ab[K, V])
-	dict.cmp = funcion_cmp[K, K]
+	dict.cmp = funcion_cmp
 	return dict
 }
 
@@ -103,6 +108,7 @@ func (dict *ab[K, V]) transplantar(nodo **nodoAb[K, V]) {
 	nuevoNodo := dict.reemplazante(&(*nodo).izq)
 	nuevaClave := nuevoNodo.clave
 	nuevoDato := dict.Borrar(nuevaClave)
+	dict.cantidad++ //Para contrarrestar el Borrar de la linea de arriba
 
 	//piso datos
 	(*nodo).clave = nuevaClave
@@ -122,11 +128,11 @@ func (dict *ab[K, V]) Guardar(clave K, dato V) {
 	dict.cantidad++
 }
 
-func (dict ab[K, V]) Pertenece(clave K) bool {
+func (dict *ab[K, V]) Pertenece(clave K) bool {
 	return *(dict.buscar(clave, &dict.raiz)) != nil
 }
 
-func (dict ab[K, V]) Obtener(clave K) V {
+func (dict *ab[K, V]) Obtener(clave K) V {
 	nodo := dict.buscar(clave, &dict.raiz)
 	if *nodo == nil {
 		panic("La clave no pertenece al diccionario")
@@ -134,7 +140,7 @@ func (dict ab[K, V]) Obtener(clave K) V {
 	return (*nodo).dato
 }
 
-func (dict ab[K, V]) Cantidad() int {
+func (dict *ab[K, V]) Cantidad() int {
 	return dict.cantidad
 }
 
@@ -160,40 +166,110 @@ func (dict *ab[K, V]) Borrar(clave K) V {
 
 }
 
-func (dict *ab[K, V]) Iterar(visitar func(K, V) bool) {
+func (dict ab[K, V]) Iterar(visitar func(K, V) bool) {
+	dict.raiz.iterar(visitar)
+}
+
+func (nodo *nodoAb[K, V]) iterar(visitar func(K, V) bool) {
+	if nodo == nil {
+		return
+	}
+	if nodo.izq != nil {
+		nodo.izq.iterar(visitar)
+	}
+	if !visitar(nodo.clave, nodo.dato) {
+		return
+	}
+	if nodo.der != nil {
+		nodo.der.iterar(visitar)
+	}
 
 }
 
 // ################################### PRIMITIVAS ITERADOR EXTERNO ################################################
+func (nodo *nodoAb[K, V]) buscarHijosIzquierdayApilar(pila TDAPila.Pila[*nodoAb[K, V]]) *nodoAb[K, V] {
+	if nodo == nil {
+		return nil
+	}
+	pila.Apilar(nodo)
+	if nodo.izq == nil {
+		return nodo
+	}
+	return nodo.izq.buscarHijosIzquierdayApilar(pila)
+}
 
 func (dict *ab[K, V]) Iterador() IterDiccionario[K, V] {
-	return
+	iter := iteradorDict[K, V]{diccionario: dict, rangoMin: nil, rangoMax: nil}
+	iter.actual = dict.raiz.buscarHijosIzquierdayApilar(iter.pilaElementos)
+	return &iter
 }
 
 func (iter *iteradorDict[K, V]) HaySiguiente() bool {
-	return
+	return !iter.pilaElementos.EstaVacia()
 }
 
 func (iter *iteradorDict[K, V]) VerActual() (K, V) {
 	if !iter.HaySiguiente() {
 		panic("El iterador termino de iterar")
 	}
-	return
+	clave, dato := iter.pilaElementos.VerTope().clave, iter.pilaElementos.VerTope().dato
+	return clave, dato
 }
 
 func (iter *iteradorDict[K, V]) Siguiente() K {
 	if !iter.HaySiguiente() {
 		panic("El iterador termino de iterar")
 	}
-	return
+	nodoActual := iter.pilaElementos.Desapilar()
+	if nodoActual.der != nil {
+		iter.pilaElementos.Apilar(nodoActual.der)
+		nodoActual.der.buscarHijosIzquierdayApilar(iter.pilaElementos)
+	}
+	return nodoActual.clave
 }
 
 // ################################### PRIMITIVAS DICCIONARIO ORDENADO #############################################
 
-func IterarRango(desde *K, hasta *K, visitar func(clave K, dato V) bool) {
+func (dictOrdenado ab[K, V]) IterarRango(desde *K, hasta *K, visitar func(clave K, dato V) bool) {
+	dictOrdenado.raiz.iterarRango(desde, hasta, visitar, dictOrdenado.cmp)
+}
+
+func (nodo *nodoAb[K, V]) iterarRango(desde *K, hasta *K, visitar func(K, V) bool, cmp funcCmp[K]) {
+	if nodo == nil {
+		return
+	}
+	if nodo.izq != nil && cmp(nodo.clave, *desde) > VALOR_CMP {
+		nodo.izq.iterar(visitar)
+	}
+	if !visitar(nodo.clave, nodo.dato) {
+		return
+	}
+	if nodo.der != nil && cmp(nodo.clave, *hasta) < VALOR_CMP {
+		nodo.der.iterar(visitar)
+	}
 
 }
 
-func IteradorRango(desde *K, hasta *K) IterDiccionario[K, V] {
+func (nodo *nodoAb[K, V]) buscarValorMinyApilar(pila TDAPila.Pila[*nodoAb[K, V]],
+	cmp funcCmp[K], desde *K) *nodoAb[K, V] {
+	if nodo == nil {
+		return nil
+	}
+	pila.Apilar(nodo)
+	if nodo.izq == nil {
+		return nodo
+	}
+	if cmp(nodo.izq.clave, *desde) < VALOR_CMP {
+		return nodo
+	}
+	return nodo.izq.buscarValorMinyApilar(pila, cmp, desde)
+}
+
+func (dictOrdenado ab[K, V]) IteradorRango(desde *K, hasta *K) IterDiccionario[K, V] {
+
+	iter := iteradorDict[K, V]{diccionario: &dictOrdenado, rangoMin: desde, rangoMax: hasta}
+	iter.actual = dictOrdenado.raiz.buscarValorMinyApilar(iter.pilaElementos, dictOrdenado.cmp, iter.rangoMin)
+
+	return &iter
 
 }
